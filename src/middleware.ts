e36d2d9ddda4daea54c302002825e038a9d6adea
arsaizdihar <arsadihar@gmail.com>
2022-07-10 17:57:09 +0700

    remove proxy

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const url = request.nextUrl;
-  console.log(request.url);
-  console.log(request.nextUrl.origin);
-  if (request.url.startsWith("https://file.arsaizdihar.com")) {
-    const oneYear = 60 * 60 * 24 * 365;
-    return NextResponse.rewrite(
-      `https://ars.is3.cloudhost.id${url.pathname}}`,
-      {
-        status: 301,
-        headers: { "Cache-Control": `public, max-age=${oneYear}` },
-      }
-    );
-  }
   if (
     url.pathname !== "/" &&
     !url.pathname.startsWith("/app") &&
