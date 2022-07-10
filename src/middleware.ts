84cb4c4c97e181349a382d75de0ce4970703b2e7
arsaizdihar <arsadihar@gmail.com>
2022-07-10 17:48:03 +0700

    check hostname

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const url = request.nextUrl;
+  console.log(url.hostname);
   if (url.hostname === "file.arsaizdihar.com") {
     const oneYear = 60 * 60 * 24 * 365;
     return NextResponse.rewrite(
